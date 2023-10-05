567560c691d0d4766a14a2948b5dab567506f4f9
kicpor <kkicpor@gmail.com>
2023-10-05 07:56:27 +0200

    a

---
+++ src/main/java/paka/Main.java
@@ -0,0 +1,17 @@
+package paka;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(myMethod());
+        ChromeOptions options = new ChromeOptions();
+        WebDriver driver = new ChromeDriver(options);
+    }
+    private static String myMethod(){
+            return "my string";
+        }
+    }
+
